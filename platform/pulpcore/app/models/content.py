d8a5b1d96bcacd301c526bd428c785818fc03415
Dennis Kliban <dkliban@redhat.com>
2017-07-14 16:22:29 -0400

    Problem: Content can not be created using REST API
    Solution: Add viewset and serializers to serve Content creation API
    
    closes #2872
    https://pulp.plan.io/issues/2872

--- platform/pulpcore/app/models/content.py
+++ platform/pulpcore/app/models/content.py
@@ -7,6 +7,40 @@ from django.db import models
 from pulpcore.app.models import Model, MasterModel, Notes, GenericKeyValueRelation
 
 
+class Artifact(Model):
+    """
+    A file associated with a piece of content.
+
+    Fields:
+
+        file (models.FileField): The stored file.
+        downloaded (models.BooleanField): The associated file has been successfully downloaded.
+        requested (models.BooleanField): The associated file has been requested by a client at
+            least once.
+        size (models.IntegerField): The size of the file in bytes.
+        md5 (models.CharField): The MD5 checksum of the file.
+        sha1 (models.CharField): The SHA-1 checksum of the file.
+        sha224 (models.CharField): The SHA-224 checksum of the file.
+        sha256 (models.CharField): The SHA-256 checksum of the file.
+        sha384 (models.CharField): The SHA-384 checksum of the file.
+        sha512 (models.CharField): The SHA-512 checksum of the file.
+    """
+    def storage_path(self, name):
+        digest = self.sha256
+        return "%s/artifacts/%s/%s" % (settings.MEDIA_ROOT, digest[0:2], digest[2:])
+
+    file = models.FileField(db_index=True, upload_to=storage_path, max_length=255)
+    downloaded = models.BooleanField(db_index=True, default=False)
+    requested = models.BooleanField(db_index=True, default=False)
+    size = models.IntegerField(blank=True, null=True)
+    md5 = models.CharField(max_length=32, blank=True, null=True)
+    sha1 = models.CharField(max_length=40, blank=True, null=True)
+    sha224 = models.CharField(max_length=56, blank=True, null=True)
+    sha256 = models.CharField(max_length=64, blank=True, null=True)
+    sha384 = models.CharField(max_length=96, blank=True, null=True)
+    sha512 = models.CharField(max_length=128, blank=True, null=True)
+
+
 class Content(MasterModel):
     """
     A piece of managed content.
@@ -24,6 +58,7 @@ class Content(MasterModel):
     natural_key_fields = ()
 
     notes = GenericKeyValueRelation(Notes)
+    artifacts = models.ManyToManyField(Artifact, through='ContentArtifact')
 
     class Meta:
         verbose_name_plural = 'content'
@@ -38,20 +73,11 @@ class Content(MasterModel):
         return tuple(getattr(self, f.name) for f in self.natural_key_fields)
 
 
-class Artifact(Model):
+class ContentArtifact(Model):
     """
-    A file associated with a piece of content.
+    A relationship between a Content and an Artifact.
 
-    Fields:
-
-        file (models.FileField): The stored file.
-        size (models.IntegerField): The size of the file in bytes.
-        md5 (models.CharField): The MD5 checksum of the file.
-        sha1 (models.CharField): The SHA-1 checksum of the file.
-        sha224 (models.CharField): The SHA-224 checksum of the file.
-        sha256 (models.CharField): The SHA-256 checksum of the file.
-        sha384 (models.CharField): The SHA-384 checksum of the file.
-        sha512 (models.CharField): The SHA-512 checksum of the file.
+    Serves as a through model for the 'artifacts' ManyToManyField in Content.
     """
     def storage_path(self, name):
         """
@@ -63,11 +89,6 @@ class Artifact(Model):
         """
         return default_storage.get_artifact_path(self.sha256)
 
-    file = models.FileField(db_index=True, upload_to=storage_path, max_length=255)
-    size = models.IntegerField(blank=False, null=False)
-    md5 = models.CharField(max_length=32, blank=False, null=False, unique=False, db_index=True)
-    sha1 = models.CharField(max_length=40, blank=False, null=False, unique=False, db_index=True)
-    sha224 = models.CharField(max_length=56, blank=False, null=False, unique=False, db_index=True)
-    sha256 = models.CharField(max_length=64, blank=False, null=False, unique=True, db_index=True)
-    sha384 = models.CharField(max_length=96, blank=False, null=False, unique=True, db_index=True)
-    sha512 = models.CharField(max_length=128, blank=False, null=False, unique=True, db_index=True)
+    artifact = models.ForeignKey(Artifact, on_delete=models.CASCADE)
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    relative_path = models.CharField(max_length=64)
